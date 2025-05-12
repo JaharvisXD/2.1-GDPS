9388c8ed959e31753f63f5cd207b6ef2c897f89c
JarvisXuanG <122207503+JaharvisXD@users.noreply.github.com>
2025-05-12 12:42:35 +0800

    Add files via upload

---
+++ website/config.php
@@ -0,0 +1,15 @@
+<?php
+$windows = "https://www.mediafire.com/file/pc2apvao4bx2qic/2.1_GDPS_Windows.zip/file";
+$android = "https://www.mediafire.com/file/i99zb2e7goqf7q3/2.1_GDPS_Android.apk/file";
+$songreupload = "https://two1gdps.ps.fhgdps.com/dashboard/reupload/songAdd.php";
+$levelreupload = "https://two1gdps.ps.fhgdps.com/dashboard/levels/levelReupload.php";
+$challengelist = "https://two1gdpscl.pages.dev/";
+$dashboard = "https://two1gdps.ps.fhgdps.com/dashboard";
+$demonlist = "https://two1gdpsdl.pages.dev/";
+$youtube = "https://www.youtube.com/@2P1GDPS";
+$gdpshub = "https://gdpshub.com/gdps/1898";
+$discord = "https://discord.gg/N8MX2mZKCF";
+$about = "Welcome to 2.1 GDPS! 2.1 GDPS is a Geometry Dash Private Server where everything is only based on the 2.1 Update released back in 2017. Thank you for joining 2.1 GDPS! We hope you enjoy your stay.";
+$browser = "https://two1gdps.ps.fhgdps.com/browser";
+$credits = "1.9 GDPS: Special Thanks and Discord button, MathieuAR: GDPSFH, MegaSa1nt: Dashboard system, Janix: idk hes a weirdo, Starmor: GDPS Website, RobTop: Making this awesome game!";
+?>
---
+++ website/index.php
@@ -0,0 +1,82 @@
+<?php
+include 'config.php';
+?>
+
+<html>
+
+<head>
+    <title>2.1 GDPS</title>
+    <link rel="stylesheet" type="text/css" href="files/style.css">
+<link href="//fonts.googleapis.com/css?family=Mukta+Mahee:200,400,700" rel="stylesheet">
+	<link href="css/stylesheet-main.css" type="text/css" rel="stylesheet">
+	<link rel="shortcut icon" href="icon.png"
+	 type="image/x-icon">
+   <link href="/fontawesome/css/all.css" rel="stylesheet">
+  <meta id="meta-title" property="og:title" content="2.1 GDPS">
+ <meta name="theme-color" content="#24b8f2"/>
+	<meta id="meta-desc" property="og:description" content="Welcome to 2.1 GDPS! Experience what 2.1 is like back then!">
+	<meta id="meta-image" name="og:image" itemprop="image" content="https://cdn.discordapp.com/attachments/880283536635076620/1350079938006487040/Untitled6_20230512132107.png?ex=67d56f6c&is=67d41dec&hm=ff1d2c0df4395e4f1a13b066c944509990ec2a8e4d39224d21dfdbd31061f979&">
+	<meta name="viewport" content="user-scalable=no">
+	<meta charset="UTF-8">
+  <script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body background= "background.png">
+
+</body>
+
+    <center>
+        <img src="logo.png" style="max-width: 70%; height: auto;">
+
+        <div>
+            <br>
+            <br>
+            <br>
+            <!-- Upper buttons -->
+            <a href="<?php echo $songreupload; ?>"><img src="files/song.png" width="130" hspace="1"></a>
+            <a href="<?php echo $levelreupload; ?>"><img src="files/reupload.png" width="130" hspace="1"></a>
+            <a href="<?php echo $browser; ?>"><img src="files/browser.png" width="130" hspace="1"></a>
+            <a href="<?php echo $dashboard; ?>"><img src="files/dashboard.png" width="130" hspace="1"></a>
+            <a href="<?php echo $android; ?>"><img src="files/android.png" width="130" hspace="1"></a>
+            <a href="<?php echo $windows; ?>"><img src="files/windows.png" width="130" hspace="1"></a>
+            <!-- Add buttons below -->
+        </div>
+
+        <br>
+
+        <div>
+            <!-- Lower buttons -->
+            <a href="<?php echo $challengelist; ?>"><img src="files/challengelist.png" width="130" hspace="1"></a>
+ <a href="<?php echo $demonlist; ?>"><img src="files/demonlist.png" width="130" hspace="1"></a>
+            <a href="<?php echo $youtube; ?>" target="_blank"><img src="files/youtube.png" width="130"
+                    hspace="1"></a>
+            <a href="<?php echo $gdpshub; ?>"><img src="files/gdpshub.png" width="130" hspace="1"></a>
+            <a href="<?php echo $discord; ?>" target="_blank"><img src="files/discord.png" width="130"
+                    hspace="1"></a>
+            <!-- Add buttons below -->
+        </div>
+        
+        <br>
+        <img src="files/about.png" style="max-width: 30%; height: auto;">
+        
+        <div class="text">
+            <span class="white"><?php echo $about; ?></span>
+        </div>
+        
+        <br>
+        <img src="files/credits.png" style="max-width: 25%; height: auto;">
+        
+        <div class="text">
+            <span class="white"><?php echo $credits; ?></span>
+        </div>
+
+        <div class="text">
+            <!-- Do not remove pls -->
+            <span class="white">Owned By <span class="lime"><u><a href="https://www.youtube.com/@JarvisXuanG"
+                            target="_blank">JarvisXD</a> & <span class="lime"><u><a href="https://youtube.com/@itscheps365"
+                            target="_blank">ChepsGames</a>
+        </div>
+    </center>
+</body>
+
+</html>
